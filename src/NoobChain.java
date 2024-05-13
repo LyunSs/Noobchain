f28f28d29dc582fb7acb1d110d9fb1fa1f278120
laurent.sun <laurent.sun@ibm.com>
2024-05-13 12:07:24 +0100

    refactor code

--- src/NoobChain.java
+++ src/NoobChain.java
@@ -6,19 +6,9 @@ public class NoobChain {
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
     public static void main(String[] args) {
 
-
-        Block genesisBlock = new Block("Hi im the first block", "0");
-        System.out.println("Hash for block 1 : " + genesisBlock.hash );
-
-        Block secondBlock = new Block("Yo im the second block",genesisBlock.hash);
-        System.out.println("Hash for block 2 : " + secondBlock.hash );
-
-        Block thirdBlock = new Block("Hey im the third block",secondBlock.hash);
-        System.out.println("Hash for block 3 : " + thirdBlock.hash);
-
-        blockchain.add(genesisBlock);
-        blockchain.add(secondBlock);
-        blockchain.add(thirdBlock);
+        blockchain.add(new Block("Hi im the first block", "0"));
+        blockchain.add(new Block("Hi im the second block", blockchain.get(blockchain.size() - 1).hash));
+        blockchain.add(new Block("Hi im the third block", blockchain.get(blockchain.size() - 1).hash));
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println(blockchainJson);
